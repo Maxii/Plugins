577d4b166b7e1946c7de8924e388327553e020fc
Maxii <jim@StrategicForge.com>
2016-11-16 17:05:35 -0500

    Incorporated ProfilerDebug configuration (changes to output folder) into CodeEnv.Master referenced projects, required by changes to MSBuild using ProfilerDebug configuration.

--- AStarTemp/AStar/Core/Serialization/SimpleJsonReplacement.cs
+++
@@ -1,7 +0,0 @@
-#if ASTAR_NO_JSON
-namespace Pathfinding.Serialization.JsonFx {
-	public class JsonMemberAttribute : System.Attribute {}
-	public class JsonOptInAttribute : System.Attribute {}
-	public class JsonNameAttribute : System.Attribute { public JsonNameAttribute (string s) {} }
-}
-#endif
